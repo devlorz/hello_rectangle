564ed9bd6c4451e6ac318f90ad3988ce66ad8121
Nutti Saelor <leelorz6@gmail.com>
2018-07-06 17:29:35 +0700

    add category widget

--- lib/main.dart
+++ lib/main.dart
@@ -32,23 +32,3 @@ void main() {
     ),
   );
 }
-
-// class HelloRectangle extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     return Center(
-//       child: Container(
-//         color: Colors.greenAccent,
-//         height: 400.0,
-//         width: 300.0,
-//         child: Center(
-//           child: Text(
-//             'Hello!',
-//             style: TextStyle(fontSize: 40.0),
-//             textAlign: TextAlign.center,
-//           ),
-//         ),
-//       ),
-//     );
-//   }
-// }
